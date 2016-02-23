001c5f36cef4c2215a52acd655f4e7fccb92d71d
minkrohmer <minkrohmer@gmail.com>
2016-02-23 21:01:06 +0100

    Added files via upload

---
+++ b64POSTrequest.php
@@ -0,0 +1,23 @@
+<?php 
+// gets data from the body of the received POST request
+$data = file_get_contents('php://input');
+// base64 encodes the body from the received POST request
+$postbody = base64_encode ($data);
+
+$url = 'http://api.mixpanel.com/track/';
+
+// settings for the POST request to mixpanel
+$options = array(
+    'http' => array(
+        'header' => "Content-Type: application/json",
+        'method' => 'POST',
+        'content' => 'data=' . $postbody,
+        ),
+    );
+$context  = stream_context_create($options);
+
+// sends a POST request to mixpanel with the base64 encoded JSON object as the request body
+$fp = fopen($url, 'r', false, $context);
+fpassthru($fp);
+fclose($fp);
+?>
